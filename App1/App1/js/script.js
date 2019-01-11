31c0657e6b013a62785c5e288803b3e4143cad07
21 Chapel <40070531+21chapel@users.noreply.github.com>
2019-01-11 01:04:59 -0500

    90%

--- App1/App1/js/script.js
+++ App1/App1/js/script.js
@@ -390,7 +390,7 @@ function PreReqsON(){
 
 function PrePreReqsON(){
     for (var i = arguments.length - 1; i >= 0; i--) {
-        $("#" + arguments[i]).css("font-weight", "bold").css("color","black").css("background-color", "lightgreen");
+        $("#" + arguments[i]).css("color","black").css("background-color", "lightgreen");
     }
 }
 
@@ -409,7 +409,7 @@ function PostPostReqsOff(){
 
 function PostPostReqsON(){
     for (var i = arguments.length - 1; i >= 0; i--) {
-        $("#" + arguments[i]).css("border","5px solid black").css("font-weight", "bold").css("color","black").css("background-color", "pink");
+        $("#" + arguments[i]).css("font-weight", "bold").css("color","black").css("background-color", "pink");
     }
 }
 
